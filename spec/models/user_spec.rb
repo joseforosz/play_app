18232295aafc3b86c0b4276f5781813dd1c1c3b9
Joe Orosz <joseforosz@gmail.com>
2012-04-01 12:56:14 -0400

    adding User tests, josef is up, stopping at 6.2.5

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,64 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+require 'spec_helper'
+
+describe User do
+
+  before do
+   @user = User.new(name: "Example User", email: "user@example.com")
+  end 
+    
+  subject {@user}
+  
+  it {should respond_to (:name)}
+  it {should respond_to (:email)}
+  it {should be_valid}
+
+  describe "when name is not present" do
+    before{@user.name=""}
+    it {should_not be_valid}
+  end
+
+  describe "when name is too long" do
+    before{@user.name = "a" * 52}
+    it {should_not be_valid}
+  end
+
+  describe "when email is not preset" do
+    before{@user.email=""}
+    it {should_not be_valid}
+  end
+
+  describe "when email format is invalid" do
+    bad_addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    bad_addresses.each do|addres|
+    before {@user.email=addres}
+    it {should_not be_valid}
+   end
+  end 
+
+ describe "when email format is valid" do
+  good_address = %w[user@foo.com A_USER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+  good_address.each do |addres|
+   before {@user.email=addres}
+   it {should be_valid}
+  end
+ end
+
+ describe "when email address is already taken" do
+  before do
+    user_with_same_email = @user.dup
+    user_with_same_email.save
+  end
+  it {should_not be_valid}
+ end
+end
