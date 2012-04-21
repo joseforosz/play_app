f9ea393093bf9454f4affd1d67812a7fcb54454e
Joe Orosz <joseforosz@gmail.com>
2012-04-21 11:55:57 -0400

    finishing 7.2.1

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Michael Hartl"
+    email    "michael@example.com"
+    password "foobaqqqqr"
+    password_confirmation "foobaqqqqr"
+  end
+end
